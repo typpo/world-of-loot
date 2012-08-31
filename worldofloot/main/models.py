60681b3352ca8704f7b78a90771bb19cb59d9686
typpo <ian@typpo.us>
2012-08-31 00:31:00 -0400

    small work

--- worldofloot/main/models.py
+++ worldofloot/main/models.py
@@ -2,14 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Pin(models.Model):
+  item = models.ForeignKey(Item)
   user = models.ForeignKey(User)
-  wowhead_id = models.CharField(max_length=10)
   wants = models.IntegerField(default=0)
   haves = models.IntegerField(default=0)
 
+class Item(models.Model):
+  id = models.CharField(max_length=10)
+  url = models.CharField(max_length=100)
+  attribution = models.CharField(max_length=100)
+  item_type = models.CharField(max_length=30)
 
 class Image(models.Model):
-  pin = models.ForeignKey(Pin)
-  attribution = models.CharField(max_length=100)
+  item = models.ForeignKey(Item)
   thumb_path = models.CharField(max_length=200)
   path = models.CharField(max_length=200)
--- worldofloot/main/views.py
+++ worldofloot/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from worldofloot.main.models import Pin
 
 def index(request):
   return recent()
@@ -9,13 +10,32 @@ def recent(request):
   })
 
 def popular_gear(request):
+  pins = Pin.objects.filter(item_type='gear').order_by('wants')
   return render_to_response('main/index.html', {
+    pins: pins,
 
   })
 
-def popular_mount(request):
+def popular_mounts(request):
+  pins = Pin.objects.filter(item_type='mount').order_by('wants')
   return render_to_response('main/index.html', {
+    pins: pins,
 
   })
 
-def
+def my_loot(request):
+  return render_to_response('main/myloot.html', {
+
+  })
+
+def vote_want(request):
+  pass
+
+def vote_have(request):
+  pass
+
+def add_item(request):
+  pass
+
+def remove_item(request):
+  pass
