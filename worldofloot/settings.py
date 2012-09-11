33ed5c0fd85c6abd58b7b79148b8d71efd31491b
typpo <ian@typpo.us>
2012-09-10 20:04:15 -0400

    change login

--- worldofloot/settings.py
+++ worldofloot/settings.py
@@ -42,7 +42,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'worldofloot',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'USER': 'django_login',                      # Not used with sqlite3.
         'PASSWORD': 'foobar',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
