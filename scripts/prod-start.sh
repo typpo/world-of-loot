1384cd66416e215d613dd61b1a617bc73011209d
Ian <typppo@gmail.com>
2012-09-16 21:29:55 +0000

    prod start script activates venv

--- scripts/prod-start.sh
+++ scripts/prod-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #PORT=8080
 #python worldofloot/manage.py run_gunicorn -b "0.0.0.0:$PORT" -w 3 -k gevent --preload
+source venv/bin/activate
 worldofloot/manage.py supervisor --daemonize
 echo 'Done.'
