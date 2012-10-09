33729318ff4a21a87418430f406e9d00aa6fa5cb
Ian <typppo@gmail.com>
2012-10-09 00:56:49 -0700

    move general item management to items.js

--- worldofloot/media/js/item_modal.js
+++ worldofloot/media/js/item_modal.js
@@ -48,60 +48,6 @@ function AddItemModal() {
       me.ShowModalDialog();
       return false;
     });
-
-    // Pin deletion
-    $(document).on('click', '.delete-pin', function() {
-      // TODO all this should go in itemmanager
-      mixpanel.track('delete pin');
-      var $e = $(this);
-      var type = $e.data('item-type');
-      var id = $e.data('item-id');
-      $.getJSON('/remove/' + type + '/' + id, function(data) {
-        //window.location.reload();
-        item_manager.RemoveItemFromPage($e.parent());
-
-      });
-    });
-
-    // Wants
-    $(document).on('click', '.js-item-want', function() {
-      mixpanel.track('item want');
-      var comment = ''; //prompt("Add a comment (or leave it blank)");
-      var item_id = $(this).data('item-id');
-      var item_type = $(this).data('item-type');
-      me.AddItem(item_id, item_type,
-        'want', comment, function(err, success) {
-        if (err) {
-          showMessage("You already added this!");
-          return;
-        }
-        showMessage("This item has been added to your wishlist.");
-        // increment counter
-        var $wants_count = $('span.' + item_id + '-' + item_type + '-wants-count');
-        $wants_count.html(parseInt($wants_count.html(), 10)+1);
-      });
-      return false;
-    });
-
-    // Haves
-    $(document).on('click', '.js-item-have', function() {
-      mixpanel.track('item have');
-      var comment = ''; //prompt("Add a comment (or leave it blank)");
-      var item_id = $(this).data('item-id');
-      var item_type = $(this).data('item-type');
-      me.AddItem(item_id, item_type,
-        'have', comment, function(err, success) {
-        if (err) {
-          showMessage("You already added this!");
-          return;
-        }
-        showMessage("This item has been added to your loot.");
-        // increment counter
-        var $haves_count = $('span.' + item_id + '-' + item_type + '-haves-count');
-        $haves_count.html(parseInt($haves_count.html(), 10)+1);
-      });
-      return false;
-    });
   }
 
   this.LoadImage = function(id, type) {
